af46676f27973e17dda578af85f8ce9a2308ea7d
brycepfuetze <brpf9496@colorado.edu>
2025-05-07 12:27:49 -0600

    added check for project part 1.1

---
+++ Functions/stiffnessCheck.m
@@ -0,0 +1,35 @@
+function [] = stiffnessCheck(ABD)
+% Take in an ABD and check if it passes the criteria for Project Part 1.1
+
+    if ABD(1,1) >= 100000; disp("Pass A_xx Criteria");
+    else; disp("Fail A_xx Criteria");
+    end
+
+
+    Ntest = 1000;
+    theta = deg2rad(linspace(0,90,Ntest));
+
+    Ayypass = 1;
+    for i = 1:Ntest
+        T = build_T(theta(i));
+        bigT = [T, zeros(3,3); zeros(3,3), T'];
+        ABDrotate = inv(bigT) * ABD * inv(bigT');
+        
+        if ABDrotate(1,1) <= 30000
+            Ayypass = 0;
+        end
+    end
+    
+    if Ayypass == true; disp("Pass A_xx Bar Criteria")
+    else; disp("Fail A_xx Bar Criteria")
+    end
+
+    if ABD(4,4) >= 25000; disp("Pass D_xx Criteria");
+    else; disp("Fail D_xx Criteria");
+    end
+
+    if ABD(5,5) >= 15000; disp("Pass D_yy Criteria");
+    else; disp("Fail D_yy Criteria");
+    end
+
+end
